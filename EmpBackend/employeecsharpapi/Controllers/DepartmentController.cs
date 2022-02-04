43c1d926692528bffb674f1e02df7471100b9302
Nikhil Kasar <71208460+nknikhikasar@users.noreply.github.com>
2022-02-04 13:22:42 +0530

    Add files via upload

---
+++ EmpBackend/employeecsharpapi/Controllers/DepartmentController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using employeecsharpapi.Model;
+using Serilog;
+
+namespace employeecsharpapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select * from
+                            dbo.Orders
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            Log.Warning("Get Method Called");
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            string query = @"
+                           insert into dbo.Orders
+                           (Id,Name,Age,City)
+                    values (@Id,@Name,@Age,@City)
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Id", dep.Id);
+                    myCommand.Parameters.AddWithValue("@Name", dep.Name);
+                    myCommand.Parameters.AddWithValue("@Age", dep.Age);
+                    myCommand.Parameters.AddWithValue("@City", dep.City);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            Log.Warning("Added a Record Successfully {@dep}",dep);
+            return new JsonResult("Added Successfully");
+        }
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            string query = @"
+                           update dbo.Orders
+                           set Name=@Name,Age=@Age,City=@City
+                           where Id=@Id
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Id", dep.Id);
+                    myCommand.Parameters.AddWithValue("@Name", dep.Name);
+                    myCommand.Parameters.AddWithValue("@Age", dep.Age);
+                    myCommand.Parameters.AddWithValue("@City", dep.City);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            Log.Warning("Update Record Successfully{@dep}", dep);
+            return new JsonResult("Update Successfully");
+        }
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                           delete from dbo.Orders
+                            where Id=@Id
+                            ";
+
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@Id", id);
+
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            Log.Warning("Delete a Record Successfully{@id}", id);
+            return new JsonResult("Deleted Successfully");
+        }
+
+    }
+}
